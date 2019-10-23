50cfcacc94b49fa7ba00698dd703d5b96e498007
StefanoBelli <sa.belli@hotmail.it>
2019-10-23 14:45:16 +0200

    added selection_sort.py0

---
+++ selection_sort.py
@@ -0,0 +1,23 @@
+def selection_sort(L):
+    n = len(L)
+    end = n - 1
+
+    for i in range(n):
+        globalMinIndex = end
+        for cur in range(i, n - 1):
+            nxt = cur + 1
+            localMinIndex = 0
+            if L[nxt] < L[cur]:
+                localMinIndex = nxt
+            else:
+                localMinIndex = cur
+
+            if L[localMinIndex] < L[globalMinIndex]:
+                globalMinIndex = localMinIndex
+
+        if globalMinIndex != i:
+            L[i], L[globalMinIndex] = L[globalMinIndex], L[i]
+
+    return L
+
+print(selection_sort([7,2,4,5,3,1]))
