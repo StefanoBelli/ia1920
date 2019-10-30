5e7838008641503f8840e8865404f90bbadd2bb7
StefanoBelli <sa.belli@hotmail.it>
2019-10-30 10:21:45 +0100

    update quick_sort.py

--- quick_sort.py
+++ quick_sort.py
@@ -1,29 +1,28 @@
 def partition(A, low, high):
-    pivot = int(low + ((high - low)/2))
-
+    pivot = A[int(low + ((high - low)/2))]
     i = low
-    j = high
+    j = high + 1
 
     while i < j:
-        if A[i] < A[pivot]:
+        while A[i] < pivot:
             i += 1
-        elif A[j] > A[pivot]:
+
+        j -= 1
+        while A[j] > pivot:
             j -= 1
+
         A[i], A[j] = A[j], A[i]
 
     return j
 
 def __quick_sort(A, low, high):
-    if low >= high:
-        return A
-
-    pivot = partition(A, low, high)
-    __quick_sort(A, low, pivot)
-    __quick_sort(A, pivot+1, high)
-
-    return A
+    if low < high:
+        pivot = partition(A, low, high)
+        __quick_sort(A, low, pivot)
+        __quick_sort(A, pivot + 1, high)
 
 def quick_sort(A):
-    return __quick_sort(A, 0, len(A) - 1)
+    __quick_sort(A, 0, len(A) - 1)
+    return A
 
-print(quick_sort([5,4,3]))
+print(quick_sort([5,4,3,5,5]))
